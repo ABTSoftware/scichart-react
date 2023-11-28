21b20abd2de3f1d0b874d2b565489381f0a08833
Jim Risen <jim.dev@scichart.com>
2023-11-28 13:17:55 +0200

    feat: add SciChartMemoryDebugWrapper helper component

---
+++ src/SciChartMemoryDebugWrapper.tsx
@@ -0,0 +1,38 @@
+import { ChangeEventHandler, MouseEventHandler, PropsWithChildren, useState } from "react";
+import { SciChartSurface, MemoryUsageHelper } from "scichart";
+
+/**
+ * SciChartMemoryDebugWrapper is used to help checking memory leaks related to SciChart.
+ * Rendering the component enables Memory Debug Mode and adds extra UI elements to allow easily mount/unmount the child components
+ *  @remarks Find more info at [Memory Leak Debugging docs](https://www.scichart.com/documentation/js/current/MemoryLeakDebugging.html)
+ */
+export function SciChartMemoryDebugWrapper(props: PropsWithChildren) {
+    SciChartSurface.autoDisposeWasmContext = true;
+    SciChartSurface.wasmContextDisposeTimeout = 0;
+    MemoryUsageHelper.isMemoryUsageDebugEnabled = true;
+
+    const [drawChart, setDrawChart] = useState(false);
+
+    const handleCheckbox: ChangeEventHandler<HTMLInputElement> = e => {
+        setDrawChart(e.target.checked);
+    };
+
+    const handleClick: MouseEventHandler<HTMLInputElement> = () => {
+        // @ts-ignore force garbage collection in Chrome if enabled with --js-flags="--expose-gc"
+        window.gc && window.gc();
+        const state = MemoryUsageHelper.objectRegistry.getState();
+        console.log("state", state);
+    };
+
+    return (
+        <>
+            <div style={{ position: "fixed", top: 0, left: 0 }}>
+                <input type="checkbox" checked={drawChart} onChange={handleCheckbox} /> Render Child Components Tree
+                <br />
+                <input type="button" onClick={handleClick} value="Log Object Registry State"></input>
+                <br />
+                {drawChart ? props.children : null}
+            </div>
+        </>
+    );
+}
